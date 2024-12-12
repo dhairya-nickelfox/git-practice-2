0b0eb5a7ef843cb15fd77e65964163e22a441b87
Dhairya_009 <dhairyabhardwaj@Nickelfoxs-MacBook-Air-3.local>
2024-12-12 13:03:35 +0530

    snsdns dsnds dsd chnages in file

---
+++ git Practice 2/git_Practice_2App.swift
@@ -0,0 +1,17 @@
+//
+//  git_Practice_2App.swift
+//  git Practice 2
+//
+//  Created by Dhairya Bhardwaj on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct git_Practice_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
